08c86561ca9a1ea889b68b74887203fdfeafa53c
satit13 <satit@nopadol.com>
2016-10-06 16:56:38 +0700

    Add - Purchase Header Comment

--- main.go
+++ main.go
@@ -2,32 +2,23 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	//"github.com/jmoiron/sqlx"
-//	m "github.com/mrtomyum/inventory/model"
+
 	x "github.com/mrtomyum/inventory/controller"
-//	"log"
-//	"net/http"
-//	"golang.org/x/crypto/bcrypt"
-//	"github.com/denisenkom/go-mssqldb"
-	//"log"
-//	"log"
 )
-
-
-
 
 
 func main() {
 
 
 	r := gin.New()
-
 	//r.GET("/", Hello)
 	r.GET("/items", x.GetItem)
 	r.POST("/item", x.InsertItem)
 	r.PUT("/item", x.UpdateItem)
 
 	// Buy transaction
+	// todo: is coding ... Not completed ...
+
 	r.POST("/buy" , x.NewBuy)
 
 	// Environment Master data part
