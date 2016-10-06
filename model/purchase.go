8d14458bb64f02b23166b86a2358178231b70a21
satit13 <satit@nopadol.com>
2016-10-06 12:42:14 +0700

    Alter table Buy Modify struct of buy

--- model/purchase.go
+++ model/purchase.go
@@ -1,17 +1,13 @@
 package model
 
 type Buy struct {
+	Id int
 	DocNo string
 	DocDate string
-	DueDate string
 	VendorId string
 	SumOfItemAmount float32
-	DiscountAmount float32
 	BeforeTaxAmount float32
 	TaxAmount float32
 	TotalAmount float32
-	TaxType int
-	IsCancel int
-	TaxRate float32
 }
 
