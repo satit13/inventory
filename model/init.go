c061807577aa9fcab35ad1d4aa15140372bc9549
satit13 <satit@nopadol.com>
2016-11-18 17:34:41 +0700

    Adding - Feature API - Cancel Purchase Tested

--- model/init.go
+++ model/init.go
@@ -2,7 +2,7 @@ package model
 
 // next we define the constants of type Day
 // we use iota to provide int values to each of the contant types
-type Doctype int64
+type Doctype int
 const (
 	BIGINBAL Doctype = 0 + iota
 	PURCHASE
--- model/stock.go
+++ model/stock.go
@@ -13,17 +13,17 @@ type Stock struct {
 }
 
 type Stockprofile struct {
-	Id int64
-	ItemId int64
-	LocationId int64
-	Qty float32
-	Itemname string
+	Id           int64
+	ItemId       int64
+	LocationId   int64
+	Qty          float32
+	Itemname     string
 	Locationname string
 
 }
 
 
-func (i *Stockprofile)GetAllStock()([]Stockprofile){
+func (i *Stockprofile)GetAllStock() ([]Stockprofile) {
 
 	log.Println("Begin Model.Stockprofile.GetAllStock")
 	dbconn := Connectdb()
@@ -39,72 +39,66 @@ func (i *Stockprofile)GetAllStock()([]Stockprofile){
 	return rs
 }
 
-func (i *Stock)UpdateStock(doctype int,sc StockCard, dbconn *sqlx.DB) {
+func (i *Stock)UpdateStock(doctype int, sc StockCard, dbconn *sqlx.DB) {
 	log.Println("< ------ Begin Stockcard.Update process")
 
 	//check existing record
-	var sql string
-	sql = `select *  from stock where itemid=? and locationid = ? `
-	log.Println(sql)
+	var Sql string
+	Sql = `select *  from stock where itemid=? and locationid = ? `
+	log.Println(Sql)
 	st := []Stock{}
 
-	dbconn.Select(&st, sql, sc.ItemId, sc.LocationId )
+	dbconn.Select(&st, Sql, sc.ItemId, sc.LocationId)
 
 	log.Printf("Check Existing Data : %v Record ", len(st))
 	//log.Printf("Exists data id : %v",st[1].Id)
 	if len(st) <= 0 {
 
-		sql = `insert into stock (itemid,locationid) values (?,?)`
-		log.Println(sql)
-		_, err := dbconn.Exec(sql,sc.ItemId, sc.LocationId )
+		Sql = `insert into stock (itemid,locationid) values (?,?)`
+		log.Println(Sql)
+		_, err := dbconn.Exec(Sql, sc.ItemId, sc.LocationId)
 		if err != nil {
 			println("Exec err:", err.Error())
 		}
 	}
 
-	println("IsCancel : ",sc.Iscancel)
-	println("Doctype  : ",sc.DocType)
+	println("IsCancel : ", sc.Iscancel)
+	println("Doctype  : ", sc.DocType)
 
 
+	// Adjust Stock table
 	switch
 	{
-		case doctype == 1:
-			println("Doctype : ",sc.DocType)
-			if sc.Iscancel == 0	{
-					sql = `update stock set qty = qty+?,amount = amount+?  where itemid=? and locationid = ?`
-					log.Println(sql)
-					_, err := dbconn.Exec(sql,
-						sc.Qty,
-						sc.Amount,
-						sc.ItemId,
-						sc.LocationId,	)
-					if err != nil {
-						println("Exec err2:", err.Error())
-					}
-				}
-			// cancel
-			if sc.Iscancel == 1 {
-				sql = `update stock set qty = qty-?,amount = amount-?  where itemid=? and locationid = ?`
-				log.Println("BEGIN IS CANCEL = 1 , PURCHASE")
-				_, err := dbconn.Exec(sql,
-					sc.Qty,
-					sc.Amount,
-					sc.ItemId,
-					sc.LocationId,	)
-					if err != nil {
-						println("Exec err2:", err.Error())
-					}
-				}
+	case doctype == 1:
+		Sql = `update stock set qty = `
+		switch sc.Iscancel {
+		case 0:
+			Sql = Sql + `qty+?,amount = amount+?  where itemid=? and locationid = ?`
+		case 1:
+			Sql = Sql + ` qty-?,amount = amount-?  where itemid=? and locationid = ?`
 		}
+		log.Println(Sql)
+		_, err := dbconn.Exec(Sql,
+			sc.Qty,
+			sc.Amount,
+			sc.ItemId,
+			sc.LocationId, )
+		if err != nil {
+			println("Exec err2:", err.Error())
+		}
+
+	// cancel
+
+	}
 
 
 
 
 	// update stock in item table
-	sql = `update item set qty = (select sum(qty) from stock where itemid = ?) ,
+	Sql = `update item set qty = (select sum(qty) from stock where itemid = ?) ,
 			amount = (select sum(amount) from stock where itemid = ?)	where id=?`
-	log.Println(sql)
-	_, err := dbconn.Exec(sql,
+	log.Println(Sql)
+	_, err := dbconn.Exec(Sql,
 		sc.ItemId,
 		sc.ItemId,
 		sc.ItemId,
